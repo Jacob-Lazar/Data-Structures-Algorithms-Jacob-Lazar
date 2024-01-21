aab6fafdf63d1db8c6d5542233e291495d3e5aec
Jacob Lazar <jlpypra@gmail.com>
2024-01-21 12:43:57 +0530

    add Dynamic_Programming_with_Comments_problem_198

--- Daily Practice Problems - LeetCode/problem_198.java
+++ Daily Practice Problems - LeetCode/problem_198.java
@@ -1,11 +1,25 @@
 //Iterative, beats 100% of java submissions
+/*
+ * We do not look forward, rather we look backwards 
+ * i.e 
+ * Up until this number, 
+ * what is the maximum sum of a subarray that ends at this number
+ */
+
 import java.util.*;
 class problem_198 {
     public int rob(int[] nums) {
+        // Stores the maximum sum of a subarray ending at the current index
         int prevMax = 0;
+        // Stores the maximum sum of a subarray ending at the previous index
         int currMax = 0;
+
         for (int x : nums) {
+            // Keep track of the previous maximum sum before updating it
             int temp = currMax;
+            /*Update the current maximum sum by considering two possibilities:
+              1. Starting a new subarray from the current number
+              2. Extending the previous subarray with the current number */
             currMax = Math.max(prevMax + x, currMax);
             prevMax = temp;
         }
